adb80848b67fef4cacd2f25af53f78a0d1d04754
klmwook <klmwook72@gmail.com>
2023-09-05 10:43:32 +0900

    typescript 기본 자료형 , 배열타입 자료형 타입

---
+++ main.ts
@@ -0,0 +1,15 @@
+//기본 타입
+let test: string = 'hello';
+let num: number = 0;
+let isFemale: boolean = true;
+
+//참조형 타입
+let like: string[] = ['game', 'music', 'youtube'];
+let dislike: Array<string> = ['study', 'homework'];
+let odd: number[] = [1, 3, 5];
+let even: Array<number> = [2, 4, 6];
+let abc: boolean[] = [true, false, true];
+let bbc: Array<boolean> = [true, false, true];
+
+//tuple : 배열에 들어가는 자료형이 다를 때 배열의 순서마다 타입지정
+let arr1: [number, string] = [3, '3'];
